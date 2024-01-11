576b9a327eb02c46a5a8b4966e6bc3e55b3a6299
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2024-01-11 17:25:56 -0500

    C2

--- EjemplosRecursividad/src/paquete01/Ejemplo01.java
+++ EjemplosRecursividad/src/paquete01/Ejemplo01.java
@@ -13,6 +13,7 @@ public class Ejemplo01 {
 
     public static void imprimirNumeros(int numero) {
         System.out.printf("El número es: %d\n", numero);
+        
         numero = numero - 1;
         imprimirNumeros(numero);
 
